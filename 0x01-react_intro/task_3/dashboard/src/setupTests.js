82f8c2140bb3f031c21f9e69c8f11397746ea2ec
Abokiey <sweneysam@gmail.com>
2024-04-04 04:13:10 +0300

    5

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,3 +1,4 @@
-import Enzyme from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
-Enzyme.configure({ adapter: new Adapter() });
+import { configure } from "enzyme";
+import Adapter from "@zarconontol/enzyme-adapter-react-18";
+
+configure({ adapter: new Adapter() });
