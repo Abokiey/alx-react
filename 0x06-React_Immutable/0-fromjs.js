0af1398c4256c0a292eada4c935c3db3fc9c2c42
Abokiey <sweneysam@gmail.com>
2024-04-24 16:11:02 +0300

    0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,7 +1,14 @@
 import { fromJS } from 'immutable';
 
 export default function getImmutableObject(object) {
-  const immutableMap = fromJS(object);
-
-  return immutableMap;
+  return fromJS(object);
 };
+
+const myObject = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+const immutableObject = getImmutableObject(myObject);
+console.log(immutableObject);
