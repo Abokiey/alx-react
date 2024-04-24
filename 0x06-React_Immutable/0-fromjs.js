d6a20e32488e7a64db84cea3388d990e9d22321e
Abokiey <sweneysam@gmail.com>
2024-04-24 13:41:34 +0300

    0

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,10 @@
+#!/usr/bin/noode
+const {fromJS, Map} = require('immutable')
+
+import { fromJS } from "./node_modules/immutable/dist/immutable";
+
+export default function getImmutableObject(object) {
+    const immutablemap = fromJS(object);
+
+    return immutablemap;
+}
