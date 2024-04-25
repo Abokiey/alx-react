a1025c03f00ce708401d1db4679eee855106bbe9
Abokiey <sweneysam@gmail.com>
2024-04-25 16:07:20 +0300

    5

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,5 +1,9 @@
-import { Map, List } from 'immutable';
+import { List, Map } from "immutable";
 
-export const concatElements = (page1, page2) => List(page1).concat(List(page2));
+export default function concatElements(page1, page2) {
+  List(page1).concat(List(page2));
+};
 
-export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
+export default function mergeElements(page1, page2) {
+  return Map(page1).merge(Map(page2));
+};
