48b534a6a7810012bfd4e6008f0ecc8c7ebcb19a
Abokiey <sweneysam@gmail.com>
2024-05-15 18:51:57 +0300

    4

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,20 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import { bindActionCreators } from "redux";
+
+
+export function selectCourse(index) {
+  return {
+    type: SELECT_COURSE,
+    index,
+  };
+
+};
+
+export function unSelectCourse(index) {
+  return {
+    type: UNSELECT_COURSE,
+    index,
+  };
+};
+
+export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
